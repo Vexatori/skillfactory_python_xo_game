6294cc2b2f92b252189933c13f65586a3ebee2d1
Vexatori <alexander.suyunov@yandex.ru>
2024-09-19 04:05:35 +0300

    print_field func added

--- xo_game.py
+++ xo_game.py
@@ -5,9 +5,18 @@ field = [
 ]
 
 
+def print_field():
+    for i in range(len(field) - 1):
+        print(" | ".join(field[i]))
+        print("---------")
+    print(" | ".join(field[len(field) - 1]))
+
+
 def can_make_a_move(i, j):
     return 0 <= i - 1 < len(field) and 0 <= j - 1 < len(field[0]) and field[i - 1][j - 1] == '-'
 
 
 def field_not_filled():
     return any([('-' in row) for row in field])
+
+
