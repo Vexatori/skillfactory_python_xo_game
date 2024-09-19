c2fa1cd50cd6dc570933dee200cc61c59fee44ce
Vexatori <alexander.suyunov@yandex.ru>
2024-09-19 04:04:46 +0300

    check field is filled func added

--- xo_game.py
+++ xo_game.py
@@ -7,3 +7,7 @@ field = [
 
 def can_make_a_move(i, j):
     return 0 <= i - 1 < len(field) and 0 <= j - 1 < len(field[0]) and field[i - 1][j - 1] == '-'
+
+
+def field_not_filled():
+    return any([('-' in row) for row in field])
